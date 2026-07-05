c36b38fef009c17839b4b3ad39d0a752a364d688
Jakub Samel <jakub.samel@stazysta.comarch.pl>
2026-07-05 16:28:57 +0200

    remove eventlet import and monkey patching from upf-controller.py

--- scripts/upf-controller.py
+++ scripts/upf-controller.py
@@ -16,9 +16,6 @@ Usage:
     ovs-vsctl set-controller br0 tcp:127.0.0.1:6653
 """
 
-import eventlet
-eventlet.monkey_patch()
-
 import logging
 logging.getLogger("ryu.base.app_manager").setLevel(logging.WARNING)
 
